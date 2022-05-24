ab46ec9c7c796e184d0eceb9f79a9189e5f0ae06
Mohab Mohsen <mohabeldiin@gmail.com>
2022-05-24 03:49:46 +0200

    handling exceptios

--- seo_se_ranking.py
+++ seo_se_ranking.py
@@ -6,28 +6,28 @@ import logging
 
 # try:
 #     from modules.temp_mail_api.TempMailAPI import TempMail
-# except (ImportError, ModuleNotFoundError) as ex:
-#     logging.error("Module TempMailAPI not found")
-#     raise ex("Module TempMailAPI not found") from ex
+# except (ImportError, ModuleNotFoundError) as e:
+#     logging.error("Module TempMailAPI not found: %s", e.__doc__)
+#     raise (f"Module TempMailAPI not found: {e.__doc__}") from e
 try:
     from modules.validators import url as url_validator
-except (ImportError, ModuleNotFoundError) as ex:
-    logging.error("Module validators not found")
-    raise ex("Module validators not found") from ex
+except (ImportError, ModuleNotFoundError) as e:
+    logging.error("Module validators not found: %s", e.__doc__)
+    raise (f"Module validators not found: {e.__doc__}") from e
 # try:
 #     import requests
-# except (ImportError, ModuleNotFoundError) as ex:
-#     logging.error("ImportError: requests")
-#     raise ex("ImportError: requests")
+# except (ImportError, ModuleNotFoundError) as e:
+#     logging.error("ImportError: requests: %s", e.__doc__)
+#     raise (f"ImportError: requests: " {e.__doc__}) from e
 try:
     from selenium import webdriver
     from selenium.common import exceptions as selenium_exceptions
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.support.ui import WebDriverWait
-except (ImportError, ModuleNotFoundError) as ex:
-    logging.error("Module selenium not found")
-    raise ex("Module selenium not found") from ex
+except (ImportError, ModuleNotFoundError) as e:
+    logging.error("Module selenium not found: %s", e.__doc__)
+    raise (f"Module selenium not found: {e.__doc__}") from e
 
 
 class SEORanking():
@@ -35,11 +35,11 @@ class SEORanking():
 
     def __init__(self, url: str):
         """foo"""
-        logger = self.__setup_loger()
-        url = self.__validate_url(logger, url)
-        driver = self.__setup_selenium_driver(logger)
+        self.__logger = self.__setup_loger()
+        url = self.__validate_url(self.__logger, url)
+        self.__driver = self.__setup_selenium_driver(self.__logger)
         api = f"https://online.seranking.com/research.competitor.html/organic/keywords?input={url}&mode=base_domain&source=eg" # pylint: disable=line-too-long
-        self.__open_seranking(logger, driver, api)
+        self.__open_seranking(self.__logger, self.__driver, api)
 
     @staticmethod
     def __setup_loger():
@@ -92,7 +92,7 @@ class SEORanking():
         """foo"""
         logger.debug("Setting up selenium")
         options = webdriver.ChromeOptions()
-        options.headless = False
+        options.headless = True
         try:
             driver = webdriver.Chrome(
                 executable_path="C:\\Program Files (x86)\\chromedriver.exe", options=options)
@@ -100,14 +100,12 @@ class SEORanking():
             try:
                 driver = webdriver.Chrome(
                     executable_path="C:\\chromedriver.exe", options=options)
-            except selenium_exceptions.WebDriverException:
-                logger.critical("Chrome driver not found")
-                raise selenium_exceptions.WebDriverException(
-                    selenium_exceptions.WebDriverException.__str__
-                    )from selenium_exceptions.WebDriverException
-            except Exception:
-                logger.critical("Exception: %s", Exception)
-                raise Exception(f"Exception: {Exception}") from Exception
+            except selenium_exceptions.WebDriverException as ex:
+                logger.critical("Chrome driver not found: %s", ex.__doc__)
+                raise (f"Chrome driver not found: {ex.__doc__}") from ex
+            except Exception as ex:
+                logger.critical("Exception: %s", ex.__doc__)
+                raise (f"Exception: {ex.__doc__}") from ex
             else:
                 driver.implicitly_wait(5)
                 logger.debug("Returning driver: %s", driver)
@@ -118,21 +116,59 @@ class SEORanking():
         """foo"""
         try:
             driver.get(url)
-        except selenium_exceptions.TimeoutException:
-            logger.critical("TimeoutException")
-            raise selenium_exceptions.TimeoutException(
-                "TimeoutException") from selenium_exceptions.TimeoutException
-        except selenium_exceptions.WebDriverException:
-            logger.critical("Unable to open SE Ranking")
-            raise selenium_exceptions.WebDriverException(
-                "Unable to open SE Ranking") from selenium_exceptions.WebDriverException
-        except Exception:
-            logger.critical("Unable to open SE Ranking")
-            raise Exception("Unable to open SE Ranking") from Exception
+        except selenium_exceptions.TimeoutException as ex:
+            logger.critical("TimeoutException: %s", ex.__doc__)
+            raise (f"TimeoutException: {ex.__doc__}") from ex
+        except selenium_exceptions.WebDriverException as ex:
+            logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking: {ex.__doc__}") from ex
+        except Exception as ex:
+            logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking: {ex.__doc__}") from ex
         else:
             driver.maximize_window()
             driver.implicitly_wait(5)
 
+    def get_seranking_data(self) -> dict:
+        """foo"""
+        data = {}
+        try:
+            data["seranking_data"] = self.__get_seranking_data(
+                self.__logger, self.__driver)
+        except selenium_exceptions.TimeoutException as ex:
+            self.__logger.critical("TimeoutException: %s", ex.__doc__)
+            raise (f"TimeoutException: {ex.__doc__}") from ex
+        except selenium_exceptions.WebDriverException as ex:
+            self.__logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking: {ex.__doc__}") from ex
+        except Exception as ex:
+            self.__logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking: {ex.__doc__}") from ex
+        else:
+            self.__logger.debug("Returning data: %s", data)
+        return data
+
+    @staticmethod
+    def __get_seranking_data(logger, driver) -> dict:
+        """foo"""
+        data = {}
+        try:
+            data["seranking_data"] = driver.find_element(By.ID,
+                "organic-keywords-table").text
+        except selenium_exceptions.NoSuchElementException as ex:
+            logger.critical("NoSuchElementException: %s", ex.__doc__)
+            raise (f"NoSuchElementException: {ex.__doc__}") from ex
+        except selenium_exceptions.WebDriverException as ex:
+            logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking{ex.__doc__}") from ex
+        except Exception as ex:
+            logger.critical("Unable to open SE Ranking: %s", ex.__doc__)
+            raise (f"Unable to open SE Ranking{ex.__doc__}") from ex
+        else:
+            logger.debug("Returning data: %s", data)
+        return data
+
 
 if __name__ == '__main__':
     seo = SEORanking("https://www.google.com")
+    print(seo.get_seranking_data())
